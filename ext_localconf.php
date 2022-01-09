426f565b18b87778c1ce792e09e134b92343d697
r3h6 <r3h6@outlook.com>
2022-01-09 15:39:34 +0100

    Add some dev config

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,4 +5,9 @@ defined('TYPO3') || die();
 (static function () {
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerTypeConverter(\R3H6\FormTranslator\Property\TypeConverters\SiteLanguageConverter::class);
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerTypeConverter(\R3H6\FormTranslator\Property\TypeConverters\ItemCollectionConverter::class);
+
+    // \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('
+    //     plugin.tx_form.settings.yamlConfigurations.100 = EXT:form_translator/Tests/Unit/Fixtures/Yaml/BaseSetup.yaml
+    //     module.tx_form.settings.yamlConfigurations.100 = EXT:form_translator/Tests/Unit/Fixtures/Yaml/BaseSetup.yaml
+    // ');
 })();
