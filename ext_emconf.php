1d58bf8d0b891c0a38b3d797dfd1357b0fc041b8
r3h6 <r3h6@outlook.com>
2022-02-10 23:11:59 +0100

    [TASK] Change version contraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,8 +11,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
-            'form' => '11.5.0-11.5.99',
+            'typo3' => '10.4.0-11.5.99',
+            'form' => '10.4.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
