d288506e632db9f38b4f7a7714215bb28c352831
r3h6 <r3h6@outlook.com>
2022-05-21 20:24:28 +0200

    [TASK] Show spinner for ajax request

--- Resources/Public/JavaScript/Mod.js
+++ Resources/Public/JavaScript/Mod.js
@@ -1,11 +1,13 @@
 define([
   'jquery',
   'TYPO3/CMS/Core/Ajax/AjaxRequest',
-  'TYPO3/CMS/Backend/Notification'
+  'TYPO3/CMS/Backend/Notification',
+  'TYPO3/CMS/Backend/Icons'
 ], function (
   $,
   AjaxRequest,
-  Notification
+  Notification,
+  Icons
 ) {
 
   $(document).on('click', '[data-save]', function () {
@@ -17,12 +19,22 @@ define([
     var $self = $(this);
     var text = $($self.data('source')).val();
     var target = $self.data('translate-to');
+
+    Icons.getIcon('spinner-circle-dark', Icons.sizes.small).then(function (markup) {
+      $self.html(markup);
+    });
+
     new AjaxRequest(TYPO3.settings.ajaxUrls.formtranslator_translate)
       .withQueryArguments({ text: text, target: target })
       .get()
       .then(async function (response) {
         const resolved = await response.resolve();
         $($self.data('target')).val(resolved.translation);
+
+        Icons.getIcon('actions-translate', Icons.sizes.small).then(function (markup) {
+          $self.html(markup);
+        });
+
       }, function (error) {
         Notification.error('', `The request failed with ${error.response.status}`);
       });
