155b476c814a478e08b11f85b443ac49138e0e94
r3h6 <r3h6@outlook.com>
2024-10-11 18:47:53 +0200

    Set version to 3.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,11 +7,11 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'R3 H6',
     'author_email' => 'r3h6@outlook.com',
     'state' => 'beta',
-    'version' => '2.0.4',
+    'version' => '3.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-13.4.99',
-            'form' => '11.5.0-12.4.99',
+            'form' => '12.4.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
